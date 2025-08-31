0ba4be83ed0bf4454f0f787168ee58c70474c6fe
jiangtengjin <jiangtengjin@appiron.cn>
2025-08-31 15:38:08 +0800

    feat:第十一期 创建团队共享空间 空间数据管理(分库分表)

--- src/main/java/com/yupi/yupicturebackend/YuPictureBackendApplication.java
+++ src/main/java/com/yupi/yupicturebackend/YuPictureBackendApplication.java
@@ -1,5 +1,6 @@
 package com.yupi.yupicturebackend;
 
+import org.apache.shardingsphere.spring.boot.ShardingSphereAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @MapperScan("com.yupi.yupicturebackend.mapper")
 @EnableAsync
-@SpringBootApplication
+@SpringBootApplication(exclude = ShardingSphereAutoConfiguration.class)
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class YuPictureBackendApplication {
 
--- src/main/java/com/yupi/yupicturebackend/service/impl/SpaceServiceImpl.java
+++ src/main/java/com/yupi/yupicturebackend/service/impl/SpaceServiceImpl.java
@@ -52,6 +52,10 @@ public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space>
     @Lazy
     private SpaceUserService spaceUserService;
 
+//    @Resource
+//    @Lazy
+//    private DynamicShardingManager dynamicShardingManager;
+
     @Resource
     private TransactionTemplate transactionTemplate;
 
@@ -112,6 +116,8 @@ public class SpaceServiceImpl extends ServiceImpl<SpaceMapper, Space>
                     result = spaceUserService.save(spaceUser);
                     ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR, "创建团队成员记录失败");
                 }
+                // 创建分表
+//                dynamicShardingManager.createSpacePictureTable(space);
                 // 返回新写入的数据 id
                 return space.getId();
 
