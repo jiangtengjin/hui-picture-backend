1703bbbe2cc7a57b3c83ef7cf129a605feee35e2
jiangtengjin <jiangtengjin@appiron.cn>
2025-12-17 17:32:51 +0800

    feat: AI 扩图记录模块 - 新增获取任务详情接口 - 新增删除任务接口

--- src/main/java/com/xhh/yupicturebackend/controller/ExpandPictureTaskController.java
+++ src/main/java/com/xhh/yupicturebackend/controller/ExpandPictureTaskController.java
@@ -2,16 +2,15 @@ package com.xhh.yupicturebackend.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xhh.yupicturebackend.common.BaseResponse;
+import com.xhh.yupicturebackend.common.DeleteRequest;
 import com.xhh.yupicturebackend.common.ResultUtils;
 import com.xhh.yupicturebackend.exception.ErrorCode;
 import com.xhh.yupicturebackend.exception.ThrowUtils;
 import com.xhh.yupicturebackend.model.dto.TaskQueryRequest;
+import com.xhh.yupicturebackend.model.entity.ExpandPictureTask;
 import com.xhh.yupicturebackend.model.vo.TaskVO;
 import com.xhh.yupicturebackend.service.ExpandPictureTaskService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -33,6 +32,32 @@ public class ExpandPictureTaskController {
         ThrowUtils.throwIf(taskQueryRequest == null, ErrorCode.PARAMS_ERROR);
         Page<TaskVO> taskList = expandPictureTaskService.getTaskList(taskQueryRequest);
         return ResultUtils.success(taskList);
+    }
+
+    /**
+     * 根据 id 查询任务
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("get/{id}")
+    public BaseResponse<TaskVO> getTaskById(@PathVariable Long id){
+        ExpandPictureTask task = expandPictureTaskService.getById(id);
+        return ResultUtils.success(TaskVO.objToVo(task));
+    }
+
+    /**
+     * 根据 id 删除任务
+     *
+     * @param deleteRequest
+     * @return
+     */
+    @PostMapping("delete")
+    public BaseResponse<Boolean> deleteTaskById(@RequestBody DeleteRequest deleteRequest){
+        ThrowUtils.throwIf(deleteRequest == null, ErrorCode.PARAMS_ERROR);
+        Long id = deleteRequest.getId();
+        ThrowUtils.throwIf(id == null || id < 1, ErrorCode.PARAMS_ERROR);
+        return ResultUtils.success(expandPictureTaskService.removeById(id));
     }
 
 }
